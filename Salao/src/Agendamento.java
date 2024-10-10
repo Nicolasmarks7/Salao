0adc6d450a623f9e472db34a1c0958d9b3f30f2a
1292321139 <1292321139@ulife.com.br>
2024-10-09 21:26:37 -0300

    agendar consultas quase pronto

---
+++ Salao/src/Agendamento.java
@@ -0,0 +1,22 @@
+public class Agendamento {
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getNomeServico() {
+        return nomeServico;
+    }
+
+    public void setNomeServico(String nomeServico) {
+        this.nomeServico = nomeServico;
+    }
+
+    private String nomeUsuario;
+    private String nomeServico;
+
+}
