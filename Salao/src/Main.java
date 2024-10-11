68055e3498f3d77ecaa6b6f0cbb3c8dd4862249e
1292321139 <1292321139@ulife.com.br>
2024-10-11 19:43:03 -0300

    agendamento "concluido"

--- Salao/src/Main.java
+++ Salao/src/Main.java
@@ -5,17 +5,5 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args) {
         new Controle();
-        /*ArrayList<String> tab = new ArrayList<>();
-        tab.add("foo");
-        tab.add("bar");
-        tab.add("gah");
-        tab.add("fbi");
-        Object[] list = tab.toArray();
-
-        System.out.println(Arrays.toString(list));
-
-        Object selecao = JOptionPane.showInputDialog(null,"asda",
-                "escolha",JOptionPane.QUESTION_MESSAGE,null,list,list[0]);
-        System.out.println(selecao);*/
     }
 }
